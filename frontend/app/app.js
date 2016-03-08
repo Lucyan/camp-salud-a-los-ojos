398ed5d6d151f5add22c88947ccf56df2cc84ba0
Leonardo Olivares <leonardo.olivares@wikot.com>
2016-03-08 13:27:47 -0300

    init

---
+++ frontend/app/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var quickSurvey = angular.module("quickSurveyApp",["ngRoute","satellizer","ui.bootstrap", "ngFileUpload", "ngProgress"]);
+
+/**
+ *  Main App Controller
+ **/
+
+quickSurvey.controller('quickSurveyController', ['$scope', function ($scope) {
+
+}]);
---
+++ frontend/app/modules/home/homeController.js
@@ -0,0 +1,10 @@
+quickSurvey.controller("homeController", function ($scope, $rootScope, User) {
+
+  $scope.anglum = {
+    url   : 'http://wikot.io/anglum',
+    title : 'Quick Survey'
+  };
+
+  $scope.message = 'Encuestas flash para la comunidad.';
+
+});
