b4fc1108e6308ce99902a8765ae0ae3ebceb5d78
Bastián Gonzalez <bastian.gonzalez@wikot.com>
2016-03-09 15:29:31 -0300

    UPDATE: Agegate view

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -8,7 +8,7 @@ var saludalosojos = angular.module("saludalosojosApp",["ngRoute","ui.bootstrap",
 
 saludalosojos.controller('mainController', ['$scope', function ($scope) {
 	$scope.$on('$routeChangeStart', function(scope, next, current) {
-		if (next.$$route.controller == 'homeController') {
+		if (next.$$route.controller == 'homeController' || next.$$route.controller == 'agegateController') {
 			$scope.home_active = true;
 		} else {
 			$scope.home_active = false;
